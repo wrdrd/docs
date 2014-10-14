8f89e99feef745076ba22922a571e9bdc7c1e95d
Wes Turner <wes@wrd.nu>
2014-10-13 19:34:30 -0500

    ENH: Add Creative Commons BY-SA 4.0 International License (#5)

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'WRD R&D Documentation'
-copyright = u'2014 WRD R&D'
+copyright = u'WRD R&D 2014'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
