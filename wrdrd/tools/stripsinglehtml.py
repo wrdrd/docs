340c898a7c1f39dfd157ecea969202adb6a55980
Wes Turner <wes@wrd.nu>
2014-10-14 07:03:33 -0500

    CLN: wrdrd.tools.stripsinglehtml: lint

--- wrdrd/tools/stripsinglehtml.py
+++ wrdrd/tools/stripsinglehtml.py
@@ -25,37 +25,39 @@ def stripsinglehtml(path='index.html'):
     bs = bs4.BeautifulSoup(contents)
     bs.find('body')
     bs = bs.find('body')
-    [x.extract() for x in bs.find_all('a', {'class':'headerlink'})]
-    elem = bs.find('div', {'class':'sphinxsidebar'})
+    [x.extract() for x in bs.find_all('a', {'class': 'headerlink'})]
+    elem = bs.find('div', {'class': 'sphinxsidebar'})
     elem and elem.extract()
-    elem = bs.find('div', {'id':'indices-and-tables'})
+    elem = bs.find('div', {'id': 'indices-and-tables'})
     elem and elem.extract()
-    [x.extract() for x in bs.find_all('div', {'class':'related'})]
+    [x.extract() for x in bs.find_all('div', {'class': 'related'})]
 
     return bs
 
-#GOOGLE_SITES_TABLE_OF_CONTENTS = ("""<div><img src="https://www.google.com/chart?chc=sites&amp;cht=d&amp;chdp=sites&amp;chl=%5B%5BTable+of+contents'%3D20'f%5Cv'a%5C%3D0'10'%3D249'0'dim'%5Cbox1'b%5CF6F6F6'fC%5CF6F6F6'eC%5C0'sk'%5C%5B'%5D'a%5CV%5C%3D12'f%5C%5DV%5Cta%5C%3D10'%3D0'%3D250'%3D297'dim'%5C%3D10'%3D10'%3D250'%3D297'vdim'%5Cbox1'b%5Cva%5CF6F6F6'fC%5CC8C8C8'eC%5C'a%5C%5Do%5CLauto'f%5C&amp;sig=TzxATGSyGo59uvdEcnpk0pMmCmI" data-type="toc" data-props="align:left;maxDepth:6;width:250;" width="250" height="300" style="display:block;text-align:left;margin-right:auto;"></div>""")
+# GOOGLE_SITES_TABLE_OF_CONTENTS = ("""<div><img src="https://www.google.com/chart?chc=sites&amp;cht=d&amp;chdp=sites&amp;chl=%5B%5BTable+of+contents'%3D20'f%5Cv'a%5C%3D0'10'%3D249'0'dim'%5Cbox1'b%5CF6F6F6'fC%5CF6F6F6'eC%5C0'sk'%5C%5B'%5D'a%5CV%5C%3D12'f%5C%5DV%5Cta%5C%3D10'%3D0'%3D250'%3D297'dim'%5C%3D10'%3D10'%3D250'%3D297'vdim'%5Cbox1'b%5Cva%5CF6F6F6'fC%5CC8C8C8'eC%5C'a%5C%5Do%5CLauto'f%5C&amp;sig=TzxATGSyGo59uvdEcnpk0pMmCmI" data-type="toc" data-props="align:left;maxDepth:6;width:250;" width="250" height="300" style="display:block;text-align:left;margin-right:auto;"></div>""")
 
-#def format_for_google_sites(bs, strip_h1=True):
-    #bs = bs.find('div', {'class': 'body'})
-    #h1 = bs.find('h1')
-    #h1.insert_after(bs4.BeautifulSoup().new_tag("div", thespot="???"))
-    #if strip_h1:
-        #h1.extract()
-        ## TODO: replace with comment
-    #p = bs.find('p')
-    #if p.text == u'Contents:':
-        #p.extract()
-    #else:
-        #raise Exception("???")  # TODO
-    #return unicode(bs).replace(
-        #'''<div thespot="???"></div>''',
-        #GOOGLE_SITES_TABLE_OF_CONTENTS)
+# def format_for_google_sites(bs, strip_h1=True):
+    # bs = bs.find('div', {'class': 'body'})
+    # h1 = bs.find('h1')
+    # h1.insert_after(bs4.BeautifulSoup().new_tag("div", thespot="???"))
+    # if strip_h1:
+        # h1.extract()
+        # TODO: replace with comment
+    # p = bs.find('p')
+    # if p.text == u'Contents:':
+        # p.extract()
+    # else:
+        # raise Exception("???")  # TODO
+    # return unicode(bs).replace(
+        # '''<div thespot="???"></div>''',
+        # GOOGLE_SITES_TABLE_OF_CONTENTS)
 
 
 import unittest
 
+
 class Test_stripsinglehtml(unittest.TestCase):
+
     def test_stripsinglehtml(self):
         filename = './test/data/singlehtml.html'
         output = stripsinglehtml(filename)
@@ -80,19 +82,19 @@ def main(*args):
         usage="%prog <path>",
         description="Strip markup from a Sphinx singlehtml HTML page")
 
-    #prs.add_option('-g', '--google-sites',
-                   #dest='google_sites',
-                   #action='store_true')
+    # prs.add_option('-g', '--google-sites',
+                   # dest='google_sites',
+                   # action='store_true')
 
     prs.add_option('-v', '--verbose',
-                    dest='verbose',
-                    action='store_true',)
+                   dest='verbose',
+                   action='store_true',)
     prs.add_option('-q', '--quiet',
-                    dest='quiet',
-                    action='store_true',)
+                   dest='quiet',
+                   action='store_true',)
     prs.add_option('-t', '--test',
-                    dest='run_tests',
-                    action='store_true',)
+                   dest='run_tests',
+                   action='store_true',)
 
     args = args and list(args) or sys.argv[1:]
     (opts, args) = prs.parse_args()
@@ -116,7 +118,7 @@ def main(*args):
 
     bs = stripsinglehtml(path)
 
-    #if opts.google_sites:
+    # if opts.google_sites:
         #bs = format_for_google_sites(bs)
 
     print(unicode(bs))
