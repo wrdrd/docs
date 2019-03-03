6cfa5227e91fac7d46a1635cbfb48f77d3c83b1c
westurner <@westurner>
2019-03-03 04:09:28 -0500

    BLD: conf.py: Copyright 2014-2019

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'WRD R&D Documentation'
-copyright = u'WRD R&D 2014 - 2016'
+copyright = u'WRD R&D 2014 - 2019'
 
 srclink_project = 'https://github.com/wrdrd/docs'
 srclink_branch = 'master'
